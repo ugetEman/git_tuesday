0d4a0f28257b6756c2cf3c234d7d6a9dea30af91
ugetEman <ugetnur@gmail.com>
2016-03-08 14:14:44 +0100

    initial add

---
+++ Git_tuesday/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Git_tuesday.Startup))]
+namespace Git_tuesday
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
